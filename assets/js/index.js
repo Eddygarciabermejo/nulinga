d7ff5ec3e2a2ebbeca4ff31f637da011196df608
Eddi García <eddi.garciab@gmail.com>
2021-06-19 09:37:03 -0500

    Entrega

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,5 +31,38 @@ function colorcalendar(p,h){
 }
 
 
+//Calendar Desktop
+
+function colorcalendardesktop(a,b){
+    let boton = document.getElementById(a);
+    let text = document.getElementById(b);
+    boton.style.backgroundColor = "#7439BD"
+    text.style.color = "white"
+
+    btn = document.getElementById('div_calen')
+    btn1 = document.getElementById('div_calen2')
+    btn2 = document.getElementById('div_calen3')
+    let btncolor = document.getElementById('btn_send')
+    let p_dise = document.getElementById('tex_desible')
+
+
+    if ( btn.style.backgroundColor == "rgb(116, 57, 189)"){
+        btncolor.style.backgroundColor = "rgb(116, 57, 189)"
+        p_dise.style.display = "block"
+    }
+
+    if(btn1.style.backgroundColor == "rgb(116, 57, 189)"){
+        btncolor.style.backgroundColor = "rgb(116, 57, 189)"
+        p_dise.style.display = "block"
+    }
+
+    if(btn2.style.backgroundColor == "rgb(116, 57, 189)" ){
+        btncolor.style.backgroundColor = "rgb(116, 57, 189)"
+        p_dise.style.display = "block"
+    }
+
+
+}
+
 
 
